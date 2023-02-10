46d4e2fa231d725d347d1ea5d11dae41ca3602c6
Edita Marija <52567746+EditaMarija@users.noreply.github.com>
2023-02-10 08:47:22 +0100

    Updates

---
+++ precache-manifest.27c730da60951d490155d23051397dd6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5b8e42be02f8e858ab98",
+    "url": "/weather-app-react/static/css/main.2a493332.chunk.css"
+  },
+  {
+    "revision": "5b8e42be02f8e858ab98",
+    "url": "/weather-app-react/static/js/main.5b8e42be.chunk.js"
+  },
+  {
+    "revision": "1c72a4ba910f5abf6db3",
+    "url": "/weather-app-react/static/js/1.1c72a4ba.chunk.js"
+  },
+  {
+    "revision": "a69d6614ab32b008890e",
+    "url": "/weather-app-react/static/js/runtime~main.a69d6614.js"
+  },
+  {
+    "revision": "d59b0a1f00e631b0458e1bea022e7920",
+    "url": "/weather-app-react/static/media/sunset.d59b0a1f.jpg"
+  },
+  {
+    "revision": "8d39372a0353c7e3d87e7b2b14e49ea0",
+    "url": "/weather-app-react/index.html"
+  }
+];
